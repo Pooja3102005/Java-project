6e5ddf6a3135f1e270c066cf6e386d105717123a
Pooja3102005 <kailaserande50@gmail.com>
2025-08-03 16:04:36 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class App {
+  static int a=5;
+  static void display()
+  {
+	  System.out.println("I am in static methode");
+  }
+  public static void main(String[] args) {
+	  System.out.println(a);
+	  display();
+  }
+}
