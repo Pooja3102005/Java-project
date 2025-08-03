c842d1f3c3b340c4958e767011912dc603589e6c
Pooja3102005 <kailaserande50@gmail.com>
2025-08-03 16:05:01 +0530

    Add files via upload

---
+++ Appex.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Appex {
+	public static void main(String[] args) {
+		System.out.println("app Running...");
+	}
+	static
+	{
+		System.out.println("app starting...");
+	}
+}
