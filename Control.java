c062f1087d4644df61f0c79671aecaf93423c169
Pooja3102005 <kailaserande50@gmail.com>
2025-08-04 11:03:20 +0530

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,22 @@
+package Demoo;
+import java.util.Scanner;
+public class Control {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number:");
+		int num= sc.nextInt();
+		if(num>0 && num<0)
+		{
+			System.out.println("the number is positive");
+		}
+		else 
+		{
+			System.out.println("the number is negative");
+		}
+		
+		
+		sc.close();
+	}
+
+}
