dbadddbb11ab67052484be1b6353056a3a06987b
Pooja3102005 <kailaserande50@gmail.com>
2025-08-04 10:59:53 +0530

    Add files via upload

---
+++ Constpos.java
@@ -0,0 +1,19 @@
+package Demoo;
+import java.util.Scanner;
+public class Constpos {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num = sc.nextInt();
+		if(num>0)
+		{
+			System.out.println("The number is positive:");
+		}
+		else {
+			System.out.println("The number is negative:");
+		}		
+		sc.close();
+	}
+
+}
