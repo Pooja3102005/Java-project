af6fc54ee52e17c79ae5104cb98b910d730ee688
Pooja3102005 <kailaserande50@gmail.com>
2025-08-06 11:23:54 +0530

    Add files via upload

---
+++ LpsumNno.java
@@ -0,0 +1,21 @@
+package Demoo;
+import java.util.Scanner;
+public class LpsumNno {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of value N:");
+	   
+		int N =sc.nextInt();
+		int sum=0;
+		for( int i=1;i<=N;i++) {
+			sum= sum+i;
+		}
+		
+			System.out.println("sum of natural number is:"+sum );
+			sc.close();
+		}
+	
+	}
+
+
