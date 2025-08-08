8fea9319f6a905ff35d21b2462584d4e627a7b69
Pooja3102005 <kailaserande50@gmail.com>
2025-08-08 15:41:25 +0530

    Add files via upload

---
+++ DoMultable.java
@@ -0,0 +1,21 @@
+package While;
+
+import java.util.Scanner;
+
+public class DoMultable {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+	   
+		int n =sc.nextInt();
+		int i=1;
+		do {
+			System.out.println(n*i);
+			i++;
+		}
+		while(i<=10);
+			sc.close();
+	}
+
+}
