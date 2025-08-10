694cf67e86f260897e4ae625062b2c076573b1b7
Pooja3102005 <kailaserande50@gmail.com>
2025-08-10 19:54:13 +0530

    Add files via upload

---
+++ MulTable.java
@@ -0,0 +1,21 @@
+package While;
+
+import java.util.Scanner;
+
+public class MulTable {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+	   
+		int n =sc.nextInt();
+		int i=1;
+		while(i<=10)
+		{
+			System.out.println(n*i);
+			i++;
+		}
+		sc.close();
+	}
+
+}
