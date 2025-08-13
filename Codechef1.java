88cb3a642213f6332cd63b6e35da7dede3b691e0
Pooja3102005 <kailaserande50@gmail.com>
2025-08-13 17:12:44 +0530

    Add files via upload

---
+++ Codechef1.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef1 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign a variable using 'var'
+	        var age = 25; 
+	        
+	        // Print the value of the variable
+	        System.out.println("My age is: " + age);
+	    }
+	}
+
+
