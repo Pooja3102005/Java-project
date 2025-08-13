bc995530123ff46aa7e54b7188b7a0706ddcb21d
Pooja3102005 <kailaserande50@gmail.com>
2025-08-13 17:08:35 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,13 @@
+package Demo2;
+
+public class Typecasting {
+	public static void main(String[] args) {
+		float a=68.9F;
+		int b=(int )a;
+		System.out.println(b);
+		int c=32;
+		double d=(double )c;
+		System.out.println(d);		
+	}
+}
+
