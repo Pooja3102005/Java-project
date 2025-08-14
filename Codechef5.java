3838ed64a68f0fef9d8272e77ab253f3b4dce3a5
Pooja3102005 <kailaserande50@gmail.com>
2025-08-14 19:11:54 +0530

    Add files via upload

---
+++ Codechef5.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef5 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign an int variable
+	        int population = 1_500_000; 
+	        
+	        // Print the value of the variable
+	        System.out.println("City Population: " + population);
+	    }
+	}
+
