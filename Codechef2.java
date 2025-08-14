8f0138f79137e2f636d01fceed6ccf0a512cfc9f
Pooja3102005 <kailaserande50@gmail.com>
2025-08-14 19:08:00 +0530

    Add files via upload

---
+++ Codechef2.java
@@ -0,0 +1,11 @@
+package Codechef;
+
+public class Codechef2 {
+	
+	    public static void main(String[] args) {
+	        byte smallNumber = 100; // Declare and assign a byte variable
+	        System.out.println("The byte value is: " + smallNumber); // Print the variable
+	    }
+	}
+
+
