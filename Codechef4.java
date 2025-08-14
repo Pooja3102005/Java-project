c4fbb7ac8c19e7d697f8c0518732f09cf6331498
Pooja3102005 <kailaserande50@gmail.com>
2025-08-14 19:10:50 +0530

    Add files via upload

---
+++ Codechef4.java
@@ -0,0 +1,18 @@
+package Codechef;
+
+public class Codechef4 {
+
+    public static void main(String[] args) {
+        // Declare and assign earthquake intensity readings
+        short regionOneIntensity = 12000;
+        short regionTwoIntensity = -5000;
+        byte regionThreeIntensity = 0;
+
+        // Print the intensity values
+        System.out.println("Region One Intensity: " + regionOneIntensity);
+        System.out.println("Region Two Intensity: " + regionTwoIntensity);
+        System.out.println("Region Three Intensity: " + regionThreeIntensity);
+    }
+}
+
+
