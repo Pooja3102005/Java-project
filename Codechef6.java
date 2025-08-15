09eda07431e92a3a075c89956eb3ccaaa1ecdcdf
Pooja3102005 <kailaserande50@gmail.com>
2025-08-15 17:35:44 +0530

    Add files via upload

---
+++ Codechef6.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef6 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign a large number using long
+	        long totalSignalsReceived = 9_876_543_210L;  // Total signals from space missions
+
+	        // Print the stored value
+	        System.out.println("Total Signals Received: " + totalSignalsReceived);
+	    }
+	}
+
+
