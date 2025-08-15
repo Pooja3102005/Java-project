1ed860cd1871e1ea9ad52ca174b81afc8ce1af4f
Pooja3102005 <kailaserande50@gmail.com>
2025-08-15 17:38:47 +0530

    Add files via upload

---
+++ Codechef8.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef8 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign a float variable
+	        float averageTemperature = 28.75f; 
+	        
+	        // Print the value of the variable
+	        System.out.println("Average Temperature: " + averageTemperature + "°C");
+	    }
+	}
+
