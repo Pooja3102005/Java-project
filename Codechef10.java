0f21eefa88e00e72bdae26ef3aa98c72b9bca752
Pooja3102005 <kailaserande50@gmail.com>
2025-08-16 21:52:00 +0530

    Add files via upload

---
+++ Codechef10.java
@@ -0,0 +1,18 @@
+package Codechef;
+
+public class Codechef10 {
+	
+	    public static void main(String[] args) {
+	        //  Declare and assign values
+	           float fuelEfficiency = 18.5f;
+	           int distanceCoverd = 150;
+	           
+		         
+	        //  Print the values
+	         System.out.println("fuel Efficiency: " + fuelEfficiency + "km/L" );
+	         System.out.println("Distance Coverd: " + distanceCoverd + "km");
+	        
+
+	    }
+	}
+
