6e00f090140ccb1452dfeef67b1749eccbda6c7f
Pooja3102005 <kailaserande50@gmail.com>
2025-08-16 21:54:38 +0530

    Add files via upload

---
+++ Codechef11.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef11 {
+	
+	    public static void main(String[] args) {
+	        //  Declare and assign precise gravitational acceleration value
+	        double gravityAcceleration = 9.80665;  // Earth's gravity in m/s² with high precision
+
+	        //  Print the value
+	        System.out.println("Gravitational Acceleration: " + gravityAcceleration + " m/(s*s)");
+	    }
+	}
+
+
