1ff9cc080f338265c9cf4b84bdf4a17986ef93c4
Pooja3102005 <kailaserande50@gmail.com>
2025-08-16 21:58:47 +0530

    Add files via upload

---
+++ Codechef13.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef13 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign a char variable
+	        char grade = 'A'; 
+	        
+	        // Print the value of the variable
+	        System.out.println("Student Grade: " + grade);
+	    }
+	}
+
