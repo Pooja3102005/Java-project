4dfa32163c3a5a27082c5d1d052a4baf4a41db83
Pooja3102005 <kailaserande50@gmail.com>
2025-08-16 21:56:33 +0530

    Add files via upload

---
+++ Codechef12.java
@@ -0,0 +1,19 @@
+package Codechef;
+
+public class Codechef12 {
+	
+	    public static void main(String[] args) {
+	        // Step 1: Declare and assign temperature (float)
+
+	         float temperatuere = 28.3f;
+	         
+	        // Step 2: Declare and assign rainfall level (double)
+	             double rainfallLevel = 125.6789;
+
+	        // Step 3: Print the weather data
+	              System.out.println("Temprature: " + temperatuere + "C");
+	              System.out.println("Rainfall Level: " +rainfallLevel + "mm");
+
+	    }
+	}
+
