1b1dec192fed9dacdf5f42d6563c54eb1be9763b
Pooja3102005 <kailaserande50@gmail.com>
2025-08-17 10:30:39 +0530

    Add files via upload

---
+++ Codechef15.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef15 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign a boolean variable
+	        boolean isLightOn = true; 
+	        
+	        // Print the value of the variable
+	        System.out.println("Is the light on? " + isLightOn);
+	    }
+	}
+
+
