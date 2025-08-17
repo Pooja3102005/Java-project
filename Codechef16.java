a7af9caac7ffa4169230af11f173157fdd301f5d
Pooja3102005 <kailaserande50@gmail.com>
2025-08-17 10:35:14 +0530

    Add files via upload

---
+++ Codechef16.java
@@ -0,0 +1,16 @@
+package Codechef;
+
+public class Codechef16 {
+	public static void main(String[] args) {
+        int goalsScored = 3, assistsMade = 2, shotsOnTarget = 5;
+
+        double player1Rating = 8.5, player2Rating = 7.9;
+
+// Print output
+System.out.println("Goals Scored: " + goalsScored);
+System.out.println("Assists Made: " + assistsMade);
+System.out.println("Shots on Target: " + shotsOnTarget);
+System.out.println("Player 1 Rating: " + player1Rating);
+System.out.println("Player 2 Rating: " + player2Rating);
+}
+}
