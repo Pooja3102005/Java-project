ee4c153477120ee553d6815ca264627294349702
Pooja3102005 <kailaserande50@gmail.com>
2025-08-17 10:29:16 +0530

    Add files via upload

---
+++ Codechef14.java
@@ -0,0 +1,22 @@
+package Codechef;
+
+public class Codechef14 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign mission details
+	            char missioncode ='A';
+	            short crewsize = 7;
+	            long distancetravel = 3000000000L;
+
+
+
+	        // Print the mission data
+	          System.out.println("Mission Code: " +missioncode);
+	          System.out.println("Crew Size: "+crewsize);
+	          System.out.println("Distance Traveled: "+ distancetravel + "km");
+
+
+	    }
+	}
+
+
