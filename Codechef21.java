3df852fc24ee8d6752b1267ce4c1b2789413f4ec
Pooja3102005 <kailaserande50@gmail.com>
2025-08-18 17:31:21 +0530

    Add files via upload

---
+++ Codechef21.java
@@ -0,0 +1,17 @@
+package Codechef;
+
+public class Codechef21 {
+	public static void main(String[] args) {
+        // Main block
+        int a = 50; // Accessible throughout the main method
+
+        {
+            // Nested block
+            int b = 25; // Only accessible within this block
+            System.out.println("Inside the block: a = " + a + ", b = " + b);
+        }
+
+        System.out.println("Outside the block: a = " + a);
+        // System.out.println(b); // Uncommenting this line will cause a compilation error
+    }
+	}
