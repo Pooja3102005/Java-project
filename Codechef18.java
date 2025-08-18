95796392fbc347bd8b9902cf083038f1ce9514b8
Pooja3102005 <kailaserande50@gmail.com>
2025-08-18 17:20:51 +0530

    Add files via upload

---
+++ Codechef18.java
@@ -0,0 +1,11 @@
+package Codechef;
+
+public class Codechef18 {
+	 public static void main(String[] args) {
+	        int num = 100; // Integer value
+	        double decimalNum = num; // Implicit conversion from int to double
+
+	        System.out.println("Integer value: " + num);
+	        System.out.println("Converted double value: " + decimalNum);
+	    }
+}
