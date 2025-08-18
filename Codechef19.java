15a8895d4680e036114498b57908b46db06a0592
Pooja3102005 <kailaserande50@gmail.com>
2025-08-18 17:23:17 +0530

    Add files via upload

---
+++ Codechef19.java
@@ -0,0 +1,21 @@
+package Codechef;
+
+public class Codechef19 {
+	
+	    public static void main(String[] args) {
+	        // Declare and assign city distance in long
+	            long cityDistance = 123456789L;
+
+	        // Implicit conversion to double
+	            double convertedDistance = cityDistance;
+
+	        // Declare and assign journey status
+	           boolean isJourneyActive = true;
+
+	        // Print all values
+	        System.out.println("Distance in Long: " + cityDistance + " km");
+	        System.out.println("Converted Distance in Double: " + convertedDistance + " km");
+	        System.out.println("Is Journey Active: " + isJourneyActive);
+	    }
+	}
+
