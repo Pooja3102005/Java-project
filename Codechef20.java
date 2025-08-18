70d362ccbddc5dc2b0992b0526ef772c58c12983
Pooja3102005 <kailaserande50@gmail.com>
2025-08-18 17:24:52 +0530

    Add files via upload

---
+++ Codechef20.java
@@ -0,0 +1,11 @@
+package Codechef;
+
+public class Codechef20 {
+	public static void main(String[] args) {
+        double price = 99.99; // Double value
+        int discountedPrice = (int) price; // Explicit conversion from double to int
+
+        System.out.println("Original Price: " + price);
+        System.out.println("Discounted Price (after conversion): " + discountedPrice);
+    }
+}
