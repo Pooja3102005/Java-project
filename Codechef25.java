133fee5ae1e7882d0519c37bf7affbbb1d50fde2
Pooja3102005 <kailaserande50@gmail.com>
2025-08-19 18:44:47 +0530

    Add files via upload

---
+++ Codechef25.java
@@ -0,0 +1,17 @@
+package Codechef;
+
+public class Codechef25 {
+	 public static void main(String[] args) {
+	        // Declare and initialize two integer variables
+	        int num1 = 10; // First number
+	        int num2 = 5;  // Second number
+
+	        // Perform addition and store the result
+	        int sum = num1 + num2; // Adding num1 and num2
+
+	        // Print the sum stored in the variable
+	        System.out.println("Sum: " + sum); // Output: Sum: 15
+
+	    }
+	}
+
