38e93173d0129f0e7a2c0eb5fe82333b25ce1cbe
Pooja3102005 <kailaserande50@gmail.com>
2025-08-19 18:47:16 +0530

    Add files via upload

---
+++ Codechef26.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class Codechef26 {
+	public static void main(String[] args) {
+        // Write your code here
+        int pushups = 40, situps = 30, squats = 20;
+        int total = pushups+situps+squats;
+        System.out.println(total);
+    }
+}
