8347625925180d990acb15cd5e18bd30a1791f32
Pooja3102005 <kailaserande50@gmail.com>
2025-08-19 18:39:17 +0530

    Add files via upload

---
+++ Codechef23.java
@@ -0,0 +1,12 @@
+package Codechef;
+
+public class Codechef23 {
+	public static void main(String[] args) {
+        final int MAX_SPEED = 120; // Declaring a constant
+
+        System.out.println("Maximum Speed Limit: " + MAX_SPEED);
+
+        // Attempting to modify MAX_SPEED will cause a compilation error
+        // MAX_SPEED = 150; // Uncommenting this line will result in an error
+    }
+}
