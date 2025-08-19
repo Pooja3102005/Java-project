1e8a35f4bf9ef7f81b625df8c0e262974b8dfad6
Pooja3102005 <kailaserande50@gmail.com>
2025-08-19 18:43:20 +0530

    Add files via upload

---
+++ Codechef24.java
@@ -0,0 +1,20 @@
+package Codechef;
+
+public class Codechef24 {
+	public static void main(String[] args) {
+        // Declaring multiple int variables in one line
+        int x, y, z; 
+
+        // Assigning values separately
+        x = 5;
+        y = 10;
+        z = 15;
+
+        // Declaring and assigning values in a single statement
+        double a = 3.5, b = 7.2, c = 9.8;
+
+        // Printing values
+        System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+        System.out.println("a: " + a + ", b: " + b + ", c: " + c);
+    }
+}
