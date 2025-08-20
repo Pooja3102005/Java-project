fd8b059098f1ba6d5ce9d1ad622bab21a2b69176
Pooja3102005 <kailaserande50@gmail.com>
2025-08-20 17:04:08 +0530

    Add files via upload

---
+++ Codechef27.java
@@ -0,0 +1,15 @@
+package Codechef;
+
+public class Codechef27 {
+	public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 15; // First number
+        int num2 = 5;  // Second number
+
+        // Perform subtraction and store the result
+        int difference = num1 - num2; // Subtracting num2 from num1
+
+        // Print the difference stored in the variable
+        System.out.println("Difference: " + difference); // Output: Difference: 10
+    }
+}
