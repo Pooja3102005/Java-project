5bb3b3895fdfc0172b549363ae75c33263487585
Pooja3102005 <kailaserande50@gmail.com>
2025-08-21 17:04:23 +0530

    Add files via upload

---
+++ Codechef35.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef35 {
+	 public static void main(String[] args) {
+	        int balance = 100;  // Initial value
+	        balance -= 30;      // Subtract 30 from balance (balance = balance - 30)
+
+	        System.out.println("Remaining Balance: " + balance);
+	        
+	        balance =- 1000;   // It is same as balance = -1000
+	        System.out.println("Again updated balance: " + balance);
+	    }
+}
