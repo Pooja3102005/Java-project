bc2fd9598c3542d66283ba675213951e505802d6
Pooja3102005 <kailaserande50@gmail.com>
2025-08-21 17:01:59 +0530

    Add files via upload

---
+++ Codechef34.java
@@ -0,0 +1,11 @@
+package Codechef;
+
+public class Codechef34 {
+public static void main(String[] args){
+        
+        double balance = 5000.50, interest = 250.75;
+        // Cook your dish here
+        balance += interest;
+        System.out.println(balance);
+    }
+}
