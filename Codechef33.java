cbf1bd3c020b621de966fe0d99bcba8a18f4e7e0
Pooja3102005 <kailaserande50@gmail.com>
2025-08-21 16:59:17 +0530

    Add files via upload

---
+++ Codechef33.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef33 {
+	public static void main(String[] args) {
+        int total = 10;  // Initial value
+        total += 5;      // Adds 5 to total (total = total + 5)
+        System.out.println("Updated Total: " + total);
+        
+        total =+ 100; // It is same as total = +100 or total = 100
+        System.out.println("Again updated Total: " + total);
+
+    }
+}
