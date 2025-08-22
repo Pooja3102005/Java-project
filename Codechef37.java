f4cfb0db55077ed50430aac0e768111e9e3e1b1d
Pooja3102005 <kailaserande50@gmail.com>
2025-08-22 17:43:49 +0530

    Add files via upload

---
+++ Codechef37.java
@@ -0,0 +1,12 @@
+package Codechef;
+
+public class Codechef37 {
+	public static void main(String[] args) {
+        int totalMarks = 200;  
+        totalMarks /= 2; // Divide totalMarks by 2 (totalMarks = totalMarks / 2)
+
+        System.out.println("Marks after division: " + totalMarks);
+
+   
+}
+}
