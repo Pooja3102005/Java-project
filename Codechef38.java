c717a970abc2a369e1bc215a5cd13d0afb9411bf
Pooja3102005 <kailaserande50@gmail.com>
2025-08-22 17:47:02 +0530

    Add files via upload

---
+++ Codechef38.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class Codechef38 {
+	 public static void main(String[] args) {
+	        int totalAmount = 17;  
+	        totalAmount %= 5; // Find the remainder when totalAmount is divided by 5
+
+	        System.out.println("Remaining Amount: " + totalAmount);
+	    }
+}
