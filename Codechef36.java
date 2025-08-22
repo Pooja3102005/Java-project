fb747c707c4b8264cf04bb6e5df1e4cedfc9ec43
Pooja3102005 <kailaserande50@gmail.com>
2025-08-22 17:41:24 +0530

    Add files via upload

---
+++ Codechef36.java
@@ -0,0 +1,12 @@
+package Codechef;
+
+public class Codechef36 {
+	public static void main(String[] args) {
+        int price = 20;  // Initial value
+        price *= 3;      // Multiply price by 3 (price = price * 3)
+
+        System.out.println("Total Price: " + price);
+
+       
+    }
+}
