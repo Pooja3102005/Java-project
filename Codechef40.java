a95002c98aefd5ba97c6601d3842b10c5c2073c2
Pooja3102005 <kailaserande50@gmail.com>
2025-08-23 17:45:24 +0530

    Add files via upload

---
+++ Codechef40.java
@@ -0,0 +1,17 @@
+package Codechef;
+
+public class Codechef40 {
+	public static void main(String[] args) {
+        // Declare an integer variable 'count' and initialize it with 5
+        int count = 5;
+
+        // Print the initial value of 'count'
+        System.out.println("Before increment: " + count);
+
+        // Post-increment operation: prints the current value of 'count' and then increments it
+        System.out.println("Using count++: " + count++);
+
+        // Print the value of 'count' after the post-increment
+        System.out.println("After increment: " + count);
+    }
+}
