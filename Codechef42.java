ab6e721d7e3db9b921958e08d75e06bd61b8beb2
Pooja3102005 <kailaserande50@gmail.com>
2025-08-23 17:50:02 +0530

    Add files via upload

---
+++ Codechef42.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class Codechef42 {
+	public static void main(String[] args) {
+        int count = 5;
+        System.out.println("Before decrement: " + count);
+        System.out.println("Using count--: " + count--);
+        System.out.println("After decrement: " + count);
+    }
+}
