2f7b89f0bd65fcd70c6723401ecb489f0be84578
Pooja3102005 <kailaserande50@gmail.com>
2025-08-23 17:47:32 +0530

    Add files via upload

---
+++ Codechef41.java
@@ -0,0 +1,11 @@
+package Codechef;
+
+public class Codechef41 {
+	public static void main(String[] args) {
+        int count = 5;
+        System.out.println("Before increment: " + count);
+        System.out.println("Using ++count: " + ++count);
+        System.out.println("After increment: " + count);
+    }
+
+}
