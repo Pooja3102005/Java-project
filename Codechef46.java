1ca0802d8321ac59945e9515bafbb79ff56d710d
Pooja3102005 <kailaserande50@gmail.com>
2025-08-24 19:26:27 +0530

    Add files via upload

---
+++ Codechef46.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class Codechef46 {
+	public static void main(String[] args) {
+        int speedLimit = 60;  // Speed limit in km/h
+        int carSpeed = 70;    // Current speed of the car
+
+        System.out.println("Over speed? " + (carSpeed > speedLimit));
+    }
+}
