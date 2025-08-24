e3115d847c5ad1a381f60737d1c4243e7f6d42f6
Pooja3102005 <kailaserande50@gmail.com>
2025-08-24 19:29:41 +0530

    Add files via upload

---
+++ Codechef48.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef48 {
+	public static void main(String[] args) {
+        int lowBatteryLevel = 20;  // Minimum battery level
+        int currentBattery = 15;   // Current battery percentage
+        
+        boolean isBatterLow = (lowBatteryLevel >currentBattery) ;// Add code here to compare
+        
+        // Write statement to print the result
+        System.out.println( "Battry low?"+isBatterLow);
+    }
+}
