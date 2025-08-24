d0c8f5540dfb02f821cd39f7f6e09c45020895d9
Pooja3102005 <kailaserande50@gmail.com>
2025-08-24 19:28:14 +0530

    Add files via upload

---
+++ Codechef47.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class Codechef47 {
+	public static void main(String[] args) {
+        int minTemperature = 10;  // Minimum safe temperature
+        int currentTemperature = 5; // Current temperature
+
+        System.out.println("Is temperature too low? " + (currentTemperature < minTemperature));
+    }
+}
