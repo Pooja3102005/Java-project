4b1a5d93d02557f7fd81872036961b8c03c04257
Pooja3102005 <kailaserande50@gmail.com>
2025-08-25 21:57:57 +0530

    Add files via upload

---
+++ Codechef49.java
@@ -0,0 +1,12 @@
+package Codechef;
+
+public class Codechef49 {
+	public static void main(String[] args) {
+        int passingMarks = 40;  // Minimum marks required to pass
+        int studentMarks = 45;  // Marks obtained by the student
+
+        boolean hasPassed = studentMarks >= passingMarks; // Store result in variable
+
+        System.out.println("Has the student passed? " + hasPassed);
+    }
+}
