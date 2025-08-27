9529611e3fccfef339419351887fc69174450c78
Pooja3102005 <kailaserande50@gmail.com>
2025-08-27 13:14:27 +0530

    Add files via upload

---
+++ Superwithmetod.java
@@ -0,0 +1,28 @@
+package Inheritance;
+
+
+class Animal{
+	void eat(){
+		System.out.println("Animal can eat");
+	}
+	void add(int a,int b)
+	{
+		int sum = a+b;
+		System.out.println(sum);
+	}
+}
+class Tiger extends Animal{
+	
+	void eat(){
+		System.out.println("Tiger Eats Meat");
+		super.add(3,6);
+	}
+}
+public class Superwithmetod {
+		
+	public static void main(String[] args) {
+		Tiger t1 = new Tiger();
+		t1.eat();
+	}
+
+}
