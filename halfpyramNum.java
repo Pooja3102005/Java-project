d1df260cb0b09ca1e4c2400400794de378748aa0
Pooja3102005 <kailaserande50@gmail.com>
2025-08-28 19:15:59 +0530

    Add files via upload

---
+++ halfpyramNum.java
@@ -0,0 +1,14 @@
+package Starpattern;
+
+public class halfpyramNum {
+	public static void main(String[] args) {
+		
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.println(j+"");
+		}
+		System.out.println();
+}
+}
+}
