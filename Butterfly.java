7f363b34089d24d47174409ca4056a915dd704f7
Pooja3102005 <kailaserande50@gmail.com>
2025-08-28 18:59:25 +0530

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,26 @@
+package Starpattern;
+
+public class Butterfly {
+public static void main(String[] args) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.println("*");
+		}
+		int space=2*(n-1);
+		for(int j=1;j<=space;j++) {
+			System.out.println("*");
+		}
+		System.out.println();
+}
+	for(int i=n;i>=1;i--) {
+		for(int j=1;j<=i;j++) {
+			System.out.println("*");
+		}
+		int space=2*(n-1);
+		for(int j=1;j<=space;j++) {
+			System.out.println("*");
+		}
+	}
+}
+}
