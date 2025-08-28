34f771d9cfca7a0672a2f6d6d52ffe321bbe48bf
Pooja3102005 <kailaserande50@gmail.com>
2025-08-28 19:12:36 +0530

    Add files via upload

---
+++ Floydtraingle.java
@@ -0,0 +1,16 @@
+package Starpattern;
+
+public class Floydtraingle {
+public static void main(String[] args) {
+	int n=5;
+	int number=1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.println(number+"");
+			number++;
+		}
+		System.out.println();
+}
+}
+}
+
