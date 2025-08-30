073cdb1f79911317bc5fbce1ea669434e31cf5a3
Pooja3102005 <kailaserande50@gmail.com>
2025-08-30 17:56:13 +0530

    Add files via upload

---
+++ Codechef55.java
@@ -0,0 +1,21 @@
+package Codechef;
+
+public class Codechef55 {
+	 public static void main(String[] args) {
+	        // Test case 1: true || true
+	        boolean case1 = true || true;
+	        System.out.println("true || true = " + case1); // Output: true
+
+	        // Test case 2: true || false
+	        boolean case2 = true || false;
+	        System.out.println("true || false = " + case2); // Output: true
+
+	        // Test case 3: false || true
+	        boolean case3 = false || true;
+	        System.out.println("false || true = " + case3); // Output: true
+
+	        // Test case 4: false || false
+	        boolean case4 = false || false;
+	        System.out.println("false || false = " + case4); // Output: false
+	    }
+}
