183bb94cd7ab4ae4fb88a52da14cf6e187e51484
Pooja3102005 <kailaserande50@gmail.com>
2025-08-31 18:28:02 +0530

    Add files via upload

---
+++ Codechef58.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class Codechef58 {
+	public static void main(String[] args) {
+        boolean isRaining = false;
+
+        // Use the NOT operator to check if it's not raining
+        boolean isNotRaining = !isRaining;
+
+        // Display the result
+        System.out.println("Is it NOT raining? " + isNotRaining); // Output: true
+    }
+}
