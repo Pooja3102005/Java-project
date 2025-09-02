d041f7a69b0fa072250cf1d354c5bcbea362a093
Pooja3102005 <kailaserande50@gmail.com>
2025-09-02 18:11:24 +0530

    Add files via upload

---
+++ Codechef61.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef61 {
+	 public static void main(String[] args) {
+	        // Declaring and initializing the username
+	        String username = "JohnDoe";
+	        String userStatus = "Active User";
+	        
+	        // Printing the stored values
+	        System.out.println("Username: " + username);
+	        System.out.println("User Status: " + userStatus);
+
+	    }
+}
