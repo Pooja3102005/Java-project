57f8b18112a4f0b3e615387c4bc3322c9d8cc6ab
Pooja3102005 <kailaserande50@gmail.com>
2025-09-03 18:11:39 +0530

    Add files via upload

---
+++ Codechef64.java
@@ -0,0 +1,15 @@
+package Codechef;
+
+public class Codechef64 {
+	public static void main(String[] args) {
+        // Declaring and initializing strings
+        String firstName = "Will";
+        String lastName = "Smith";
+
+        // Concatenating using concat() method
+        String fullName = firstName.concat(" ").concat(lastName);
+
+        // Printing values
+        System.out.println("Full Name: " + fullName); // Output: Full Name: Will Smith
+    }
+}
