5dfb121a93229d2dedae5029336033b4cfb36f08
Pooja3102005 <kailaserande50@gmail.com>
2025-09-06 17:19:12 +0530

    Add files via upload

---
+++ Codechef73.java
@@ -0,0 +1,19 @@
+package Codechef;
+
+public class Codechef73 {
+	public static void main(String[] args) {
+        String word = "JavaProgramming";
+
+        // Access the first character
+        char firstChar = word.charAt(0);
+        System.out.println("First character: " + firstChar);
+
+        // Access the fifth character
+        char fifthChar = word.charAt(4);
+        System.out.println("Fifth character: " + fifthChar);
+
+        // Access the last character
+        char lastChar = word.charAt(word.length() - 1);
+        System.out.println("Last character: " + lastChar);
+    }
+}
