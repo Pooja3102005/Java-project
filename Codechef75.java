f526ab6d169ab603b8b2ba7c9b9c3f03e4c6aa26
Pooja3102005 <kailaserande50@gmail.com>
2025-09-07 17:24:07 +0530

    Add files via upload

---
+++ Codechef75.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef75 {
+	 public static void main(String[] args) {
+	        // Initialize a string with a sample text
+	        String text = "Hello, World!";
+
+	        // Extract a substring starting from index 7 ("World!")
+	        String result = text.substring(7);
+
+	        // Print the extracted substring
+	        System.out.println("Extracted Substring: " + result);
+	    }
+}
