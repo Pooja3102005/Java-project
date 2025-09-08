f49a52abb702eb0633cf5ed34e44d704ced690c6
Pooja3102005 <kailaserande50@gmail.com>
2025-09-08 18:05:26 +0530

    Add files via upload

---
+++ Codechef79.java
@@ -0,0 +1,20 @@
+package Codechef;
+
+public class Codechef79 {
+	public static void main(String[] args) {
+        // Define a string
+        String sentence = "Learning Java is fun!";
+
+        // Find the index of the first occurrence of the character 'J'
+        int indexOfJ = sentence.indexOf('J');
+
+        // Print the index
+        System.out.println("The index of 'J' is: " + indexOfJ);
+
+        // Find the index of the first occurrence of the character 'a'
+        int indexOfA = sentence.indexOf('a');
+
+        // Print the index
+        System.out.println("The index of 'a' is: " + indexOfA);
+    }
+}
