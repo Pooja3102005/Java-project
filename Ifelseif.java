857c54187580ad6cad31b8a5b79d5d4ff2579a3e
Pooja3102005 <kailaserande50@gmail.com>
2025-09-08 18:12:44 +0530

    Add files via upload

---
+++ Ifelseif.java
@@ -0,0 +1,21 @@
+package Codechef;
+
+public class Ifelseif {
+	public static void main(String[] args) {
+        // Initialize the variable
+        int number = 25;  
+        
+        // Check if the number is greater than 0
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } 
+        // Check if the number is less than 0
+        else if (number < 0) {
+            System.out.println("The number is negative.");
+        } 
+        // If the number is neither positive nor negative (i.e., it is 0)
+        else {
+            System.out.println("The number is zero."); 
+        }
+    }
+}
