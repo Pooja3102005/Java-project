d7191fa6aa150446692154be512604c8f7aa59c6
Pooja3102005 <kailaserande50@gmail.com>
2025-09-13 19:25:57 +0530

    Add files via upload

---
+++ Codechef85.java
@@ -0,0 +1,18 @@
+package Codechef;
+
+public class Codechef85 {
+	public static void main(String[] args) {
+        // Initialize a string variable
+        String greeting = "Hello, World!";
+        
+        // Check if the string starts with "Hello"
+        boolean startsWithHello = greeting.startsWith("Hello"); // Returns true
+
+        // Check if the string starts with "hi"
+        boolean startsWithhi = greeting.startsWith("hi"); // Returns false
+
+        // Print the results
+        System.out.println("Starts with 'Hello': " + startsWithHello); 
+        System.out.println("Starts with 'hi': " + startsWithhi);  
+    }
+}
