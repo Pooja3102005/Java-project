907ee524f8739dc62ca4afe71943656e48ea676b
Pooja3102005 <kailaserande50@gmail.com>
2025-09-16 18:26:31 +0530

    Add files via upload

---
+++ Codechef87.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef87 {
+	 public static void main(String[] args) {
+	        // Declare and initialize a string
+	        String originalString = "hello world";
+
+	        // Convert the string to uppercase using toUpperCase() method
+	        String upperString = originalString.toUpperCase();
+
+	        // Print the result
+	        System.out.println("Uppercase Message: " + upperString);
+	    }
+}
