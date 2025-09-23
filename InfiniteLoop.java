828498d8dd7d0c5e09d4742bfb2ddf72b3e0ed05
Pooja3102005 <kailaserande50@gmail.com>
2025-09-23 19:05:02 +0530

    Add files via upload

---
+++ InfiniteLoop.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class InfiniteLoop {
+	 public static void main(String[] args) {
+	     
+	       // This infinite loop simulates a continuously running server.
+	        while(true)
+	        {
+	             System.out.println("Server is running");
+	             //Write break; below
+	        }
+	    
+	    }
+}
