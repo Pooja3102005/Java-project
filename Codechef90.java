fd2de45d26ef8dc4922bcb55938a6a9897aa6154
Pooja3102005 <kailaserande50@gmail.com>
2025-09-28 17:57:45 +0530

    Add files via upload

---
+++ Codechef90.java
@@ -0,0 +1,15 @@
+package Codechef;
+
+public class Codechef90 {
+	 public static void main(String[] args) {
+	        
+	        // Define a variable productName and its value 
+	        String productName = "Samsung GALAXY Phone";
+	        
+	        // Use toLowerCase() method to print the product name in lowercase for storage
+	        String formattedProduct = productName.toLowerCase();
+	        
+	        // Print the desired results
+	         System.out.println("Formatted  product Name: " +formattedProduct);
+	    }
+}
