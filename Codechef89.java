47dde485ef2912f7a8903b74137b379589935d0d
Pooja3102005 <kailaserande50@gmail.com>
2025-09-28 17:56:30 +0530

    Add files via upload

---
+++ Codechef89.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef89 {
+	 public static void main(String[] args) {
+	        // Declare and initialize a string
+	        String message = "HELLO WORLD";
+
+	        // Convert the string to lowercase
+	        String lowerMessage = message.toLowerCase();
+
+	        // Print the result
+	        System.out.println("Lowercase Message: " + lowerMessage);
+	    }
+}
