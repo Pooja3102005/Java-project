0fc0b4c441b14712c02f7223cd374a26e722a479
Pooja3102005 <kailaserande50@gmail.com>
2025-09-28 17:55:22 +0530

    Add files via upload

---
+++ Codechef88.java
@@ -0,0 +1,24 @@
+package Codechef;
+
+public class Codechef88 {
+public static void main(String[] args) {
+        
+        String s1="Hello";
+        String s2="World";
+        
+        // Converting both strings to uppercase
+         s1 = s1.toUpperCase();
+         s2 = s2.toUpperCase();
+
+
+        // Concatenating with a space in between
+            String finalString =s1 + " " + s2;
+
+        // Printing the length of the final string
+           System.out.println(finalString.length());
+
+        // Printing the final string
+           System.out.println(finalString);
+        
+    }
+}
