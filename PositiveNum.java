62103643604f458b653acac39195bf9636efb3a6
Pooja3102005 <kailaserande50@gmail.com>
2025-09-29 17:56:00 +0530

    Add files via upload

---
+++ PositiveNum.java
@@ -0,0 +1,20 @@
+package Codechef;
+
+public class PositiveNum {
+	 public static void main(String[] args) {
+	        // Declare an integer variable and assign a value
+	        int number = -5;
+
+	        // Check if the number is greater than 0
+	        if (number > 0) {
+	            // If true, print that the number is positive
+	            System.out.println("The number is positive.");
+	        } else {
+	            // If false, print that the number is not positive
+	            System.out.println("The number is not positive.");
+	        }
+
+	        // This statement runs regardless of the if-else condition
+	        System.out.println("This statement always executes.");
+	    }
+}
