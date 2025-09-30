99736fd4642005fa688cb99d464609d48eb924ff
Pooja3102005 <kailaserande50@gmail.com>
2025-09-30 21:22:19 +0530

    Add files via upload

---
+++ Checknum.java
@@ -0,0 +1,16 @@
+package Codechef;
+
+public class Checknum {
+	public static void main(String[] args) {
+        // Declare and initialize an integer variable
+        int number = 10;
+
+        // If statement to check if the number is greater than 0
+        if (number > 0) {
+            // If the condition is true, print the message
+            System.out.println("The number is positive.");
+        }
+        // This statement will execute regardless of the if condition
+        System.out.println("This statement always executes.");
+    }
+}
