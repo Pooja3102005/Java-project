d106027f29915650d86a0107fcd8beb053207602
Pooja3102005 <kailaserande50@gmail.com>
2025-09-30 21:31:56 +0530

    Add files via upload

---
+++ Codechef93.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef93 {
+	 public static void main(String[] args) {
+	        // Declare and initialize a string
+	        String text = "banana";
+
+	        // Replace all occurrences of 'a' with 'o' using the replace method
+	        String replacedText = text.replace('a', 'o');
+
+	        // Print the result
+	        System.out.println("Modified Text: " + replacedText);
+	    }
+}
