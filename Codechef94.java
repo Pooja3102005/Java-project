dbdc4c12ccc3f8d1c06ca9977c8e83ea196bd5ba
Pooja3102005 <kailaserande50@gmail.com>
2025-10-02 15:08:40 +0530

    Add files via upload

---
+++ Codechef94.java
@@ -0,0 +1,15 @@
+package Codechef;
+
+public class Codechef94 {
+	  public static void main(String[] args) {
+	        
+	        // Define a variable named subjectName and provide it a value.
+	        String subjectName = "Mathematics@Physics@Chemistry";
+	        
+	        // Use replace() method to replace '@' with '_'
+	        String modifiedSubectName = subjectName.replace('@','_');
+
+	        // Print the desired results
+	        System.out.println("Modified subject name: " +modifiedSubectName);
+	    }
+}
