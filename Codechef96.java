8dd092345bcd713015a6c3ba0b61865180eb3213
Pooja3102005 <kailaserande50@gmail.com>
2025-10-05 04:46:11 -0700

    Add files via upload

---
+++ Codechef96.java
@@ -0,0 +1,14 @@
+package Codechef;
+
+public class Codechef96 {
+	public static void main(String[] args) {
+        // Declare and initialize a string
+        String sentence = "I love Python programming";
+
+        // Replace "Python" with "Java"
+        String modifiedSentence = sentence.replace("Python", "Java");
+
+        // Print the result
+        System.out.println("Modified Sentence: " + modifiedSentence);
+    }
+}
