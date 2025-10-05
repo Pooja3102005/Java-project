989ff9c7a5388325210b572fa4fdeb155ee7d1e6
Pooja3102005 <kailaserande50@gmail.com>
2025-10-05 04:46:46 -0700

    Add files via upload

---
+++ Codechef97.java
@@ -0,0 +1,13 @@
+package Codechef;
+import java.util.Scanner;
+public class Codechef97{
+	    public static void main(String[] args) {
+	    	Scanner scanner = new Scanner(System.in);
+	        System.out.println("Oh, " + scanner.nextInt() + " is a great choice!");
+	        
+	        
+	        // Close the Scanner to free up resources
+	        scanner.close();
+	    }
+	}
+
