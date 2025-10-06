fb34754e6c98fa1cd99e3e892aac4be01da65b18
Pooja3102005 <kailaserande50@gmail.com>
2025-10-06 05:58:15 -0700

    Add files via upload

---
+++ StarttoEnd.java
@@ -0,0 +1,13 @@
+package Codechef;
+
+public class StarttoEnd {
+	public static void main(String[] args) {
+        int start = 2; // Starting value of the range
+        int end = 5; // Ending value of the range
+
+        // Loop from start to end, printing each number
+        for (int i = start; i <= end; i++) {
+            System.out.println(i); // Print the current value of i
+        }
+    }
+}
