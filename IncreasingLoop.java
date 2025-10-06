e1e9bc2e9ab87e077bdaffc37aac66434463077b
Pooja3102005 <kailaserande50@gmail.com>
2025-10-06 05:59:09 -0700

    Add files via upload

---
+++ IncreasingLoop.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+public class IncreasingLoop {
+	 public static void main(String[] args) {
+	        // Loop from 2 to 10, increasing by 2 in each step
+	        for (int i = 2; i <= 10; i += 2) {
+	            System.out.println(i); // Print the current value
+	        }
+	    }
+}
