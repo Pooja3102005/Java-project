68780dfd228c95d507f17578e4a0b6ecc696b4e4
Pooja3102005 <kailaserande50@gmail.com>
2025-10-13 06:41:27 -0700

    Add files via upload

---
+++ FileContentReverse.java
@@ -0,0 +1,33 @@
+package Inheritance;
+import java.io.FileInputStream;
+
+public class FileContentReverse {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("pp.txt");
+            String str = "";
+            int ch;
+            System.out.println("Original Content of File :\n");
+
+            while ((ch = fin.read()) != -1) {
+                System.out.printf("%c", ch);
+                str += (char) (ch);
+            }
+            // reverse File Content
+            StringBuffer sbf = new StringBuffer(str);
+
+            System.out.println("\n\nReverse File Content: \n" + sbf.reverse());
+
+            System.out.println("\nOriginal file content in UpperCase: \n" + str.toUpperCase());
+            fin.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+}
+
