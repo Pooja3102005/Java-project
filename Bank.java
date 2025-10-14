4f8ac94e8722af9726485bc5a5efdf1da78a8d34
Pooja3102005 <kailaserande50@gmail.com>
2025-10-14 06:00:22 -0700

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,23 +1,43 @@
-package Sample;
-import java.util.Scanner;
+package Practical;
+import java.util.*;
 public class Bank {
-static String Bankname="IDFC";
-double balance;
-void showbankname() {
-	System.out.println("Bankname:"+Bankname);
-}
-void displaybalance() {
-	boolean balance=true;
-	System.out.println("Current balance:"+balance);
-}
-public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	Bank b1=new Bank();
-	System.out.print("Enter your balance");
-	b1.balance=sc.nextDouble();
-	b1.displaybalance();
-	sc.close();
-	
-	
-}
+    String name;
+    int acc;
+    double bal;
+    Bank(String n, int a, double b) {
+        name = n;
+        acc = a;
+        bal = b;
+    }
+    void dep(double amt) {
+        bal += amt;
+        System.out.println("Deposited: " + amt);
+    }
+    void wd(double amt) {
+        if (amt > bal)
+            System.out.println("Insufficient balance!");
+        else {
+            bal -= amt;
+            System.out.println("Withdrawn: " + amt);
+        }
+    }
+    void show() {
+        System.out.println("\nName: " + name + "\nAcc No: " + acc + "\nBalance: " + bal);
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Name: ");
+        String n = s.nextLine();
+        System.out.print("Acc No: ");
+        int a = s.nextInt();
+        System.out.print("Balance: ");
+        double b = s.nextDouble();
+        
+        Bank b1 = new Bank(n, a, b);
+        System.out.print("Deposit: ");
+        b1.dep(s.nextDouble());
+        System.out.print("Withdraw: ");
+        b1.wd(s.nextDouble());
+        b1.show();
+    }
 }
