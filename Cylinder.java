400086adc5b5c0682c79d3b016d6f27c7dd7b886
Pooja3102005 <kailaserande50@gmail.com>
2025-10-18 06:48:37 -0700

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,20 @@
+package Practical;
+interface Operation {
+double volume();
+double PI = 3.142;
+}
+class Cylinder implements Operation {
+private double radius;
+private double height;
+public Cylinder(double radius, double height) {
+    this.radius = radius;
+    this.height = height;
+}
+public double volume() {
+    return PI * radius * radius * height;
+}
+public static void main(String[] args) {
+    Cylinder cylinder = new Cylinder(5, 10);
+    System.out.println("Volume of cylinder: " + cylinder.volume());
+}
+}
