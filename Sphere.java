d5ee6733503923e63a9c0ed272ee7e7c8e9aad81
Pooja3102005 <kailaserande50@gmail.com>
2025-10-31 05:56:23 -0700

    Add files via upload

---
+++ Sphere.java
@@ -0,0 +1,14 @@
+package Practical;
+import java.util.Scanner;
+public class Sphere {
+	
+	    public static void main(String[] args) {
+	        double r = 5;
+	        double area = 4 * 3.14 * r * r;
+	        double volume = (4.0 / 3) * 3.14 * r * r * r;
+
+	        System.out.println("Surface Area: " + area);
+	        System.out.println("Volume: " + volume);
+	    }
+	}
+
